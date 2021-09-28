c88a27b0c35581345de5da11b8c99c08a5606612
Weston Isheim <wisheim@gmail.com>
2021-09-28 11:55:54 -0600

    added rollbar to server

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+    accessToken: '7a5a8aa055c043c48247fa4f80b42722',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('Html was served successfully!')
 })
 
 
 
 const port = process.env.PORT || 5656
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, ()=> console.log(`Pikachu use Thunderbolt on port: ${port}!`))
